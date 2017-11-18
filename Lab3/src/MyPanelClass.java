d99d0f298e3b1f69ba8b41f54f236739accb250d
Cristian Ruiz <cristian.ruiz1@upr.edu>
2017-11-18 17:10:52 -0400

    Added:
    Code to draw centered circle 
    commented out the code to draw borders and two diagonal lines

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -4,7 +4,8 @@ import java.awt.Insets;
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
-            /**
+
+			/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
@@ -17,10 +18,10 @@ public class MyPanelClass extends JPanel {
                         int x1 = myInsets.left;
                         int y1 = myInsets.top;
                         int x2 = getWidth() - myInsets.right - 1;
-                        int y2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.right - 1;
                         //second diagonal line coordinates
-                        int x3 = getWidth();
-                        int y3 = getHeight() - myInsets.bottom - 1;
+                        int x3 = myInsets.left;
+                        int y3 = getHeight() - myInsets.left ;
                         int x4 = getWidth() - myInsets.right - 1;
                         int y4 = myInsets.top;
                         int width = x2 - x1;
@@ -29,17 +30,20 @@ public class MyPanelClass extends JPanel {
                         //Paint the background
                         g.setColor(Color.YELLOW);
                         g.fillRect(x1, y1, width + 1, height + 1);
-                        //Draw a border
-                        g.setColor(Color.BLUE);
-                        g.drawRect(x1, y1, width, height);
-                        //Draw a second border inside the first one
-                        g.setColor(Color.RED);
-                        g.drawRect(x1 + 3, y1 + 3, width - 6, height - 6);
-                        //Draw a diagonal line
-                        g.setColor(Color.GREEN);
-                        g.drawLine(x1, y1, x2, y2);
-                        //Draw a second diagonal line
-                        g.setColor(Color.BLACK);
-                        g.drawLine(x3, y3, x4, y4);
+                        //Draw a circle
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval(x1 + (getWidth() - 55)/2, y1 + (getHeight() - 55)/2, 55, 55);
+//                        //Draw a border
+//                        g.setColor(Color.BLUE);
+//                        g.drawRect(x1, y1, width, height);
+//                        //Draw a second border inside the first one
+//                        g.setColor(Color.RED);
+//                        g.drawRect(x1 + 3, y1 + 3, width - 6, height - 6);
+//                        //Draw a diagonal line
+//                        g.setColor(Color.GREEN);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        //Draw a second diagonal line
+//                        g.setColor(Color.BLACK);
+//                        g.drawLine(x3, y3, x4, y4);
             }
 }
